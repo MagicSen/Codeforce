9bfcd0298b5e992d1660c464a688f32a71d79412
MagicSen <magicys520@gmail.com>
2014-06-18 16:54:55 +0800

    Add new file

---
+++ 225_A_Milking_Cows/test.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <algorithm>
+#include <numeric>
+#include <vector>
+#include <list>
+#include <map>
+#include <set>
+
+using namespace std;
+
+int main()
+{
+	map<string, vector<int>> temp_map;
+	return 0;
+}
